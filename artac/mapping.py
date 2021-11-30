128d758641bf3661275533e700ca86e384fb4c05
Ian Nesbitt <ian.nesbitt@gmail.com>
2021-11-30 15:57:00 -0500

    adding arrow to highlighted line in plot

--- artac/mapping.py
+++ artac/mapping.py
@@ -100,6 +100,39 @@ def get_lines(ifn):
     return lines, line, dbd
 
 
+def add_arrow(line, position=None, direction='right', size=15, color=None):
+    """
+    add an arrow to a line.
+
+    line:       Line2D object
+    position:   x-position of the arrow. If None, mean of xdata is taken
+    direction:  'left' or 'right'
+    size:       size of the arrow in fontsize points
+    color:      if None, line color is taken.
+    """
+    if color is None:
+        color = line.get_color()
+
+    xdata = line.get_xdata()
+    ydata = line.get_ydata()
+
+    if position is None:
+        position = xdata.mean()
+    # find closest index
+    start_ind = np.argmin(np.absolute(xdata - position))
+    if direction == 'right':
+        end_ind = start_ind + 1
+    else:
+        end_ind = start_ind - 1
+
+    line.axes.annotate('',
+        xytext=(xdata[start_ind], ydata[start_ind]),
+        xy=(xdata[end_ind], ydata[end_ind]),
+        arrowprops=dict(arrowstyle="->", color=color),
+        size=size
+    )
+
+
 def plot_mark(extents, m):
     x, y = m(extents['cn_lon'], extents['cn_lat'])
     m.plot(x, y, 'ro')
@@ -108,7 +141,10 @@ def plot_mark(extents, m):
 def plot_lines(lines, m, color='k'):
     for line in lines:
         x, y = m(lines[line]['lons'], lines[line]['lats'])
-        m.plot(x, y, color=color)
+        l = m.plot(x, y, color=color)
+        if color == 'red':
+            # the highlighted line
+            add_arrow(line=l)
 
 
 def drawmap(ifn, ffn, out, projects, p):
